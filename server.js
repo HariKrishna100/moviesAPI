9d18b07a3e2a09ff4dff58bb913a25489ab29e16
Harikrishna Paresh Patel <hpatel296@myseneca.ca>
2023-05-19 22:52:44 -0400

    added cite

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+* WEB422 – Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Harikrishna Paresh Patel Student ID: 150739217 Date: 19/05/2023
+* Cyclic Link: https://repulsive-shrug-crow.cyclic.app/
+*
+********************************************************************************/
+
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
